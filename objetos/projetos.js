c3142c70cba827f9914555f1e91b0f72952a6804
Vitor <vitorferreirafranca@gmail.com>
2025-01-03 19:48:35 -0300

    passando o objeto expecifico para pagina projeto por json

--- objetos/projetos.js
+++ objetos/projetos.js
@@ -1,35 +1,51 @@
+
+
 const projetos = [
   {
     imagem: 'imgs/Memoro.png',
     link: 'https://www.youtube.com/watch?v=-ttHHnTZ80M&t=90s',
     nome: 'Memoro',
-    texto: 'loren'
+    texto: 'loren',
+    numero:1
   },
   {
     imagem: 'https://exemplo.com/imagem2.jpg',
     link: 'https://exemplo2.com',
     nome: 'Projeto 2',
-    texto: 'Descrição do Projeto 2'
+    texto: 'Descrição do Projeto 2',
+    numero:2
   }
 ];
 
-        // Seleciona o container onde os itens serão exibidos
-    const container = document.getElementById('MeusProjetos');
+function projeto(objeto) {
+  let projetoSerializado = encodeURIComponent(JSON.stringify(objeto));
+  window.location.href = `projeto.html?projeto=${projetoSerializado}`;
+}
+// Seleciona o container onde os itens serão exibidos
+const container = document.getElementById('MeusProjetos');
 
-        // Itera sobre os objetos e cria uma div para cada um
-    projetos.forEach(projeto => {
-            // Cria uma nova div
+// Verifica se o container foi encontrado
+if (!container) {
+  console.error('Elemento com ID "MeusProjetos" não encontrado no DOM.');
+} else {
+  // Itera sobre os objetos e cria uma div para cada um
+  projetos.forEach(exemplo => {
+    // Cria uma nova div
     const div = document.createElement('div');
 
-            // Define o conteúdo da div
-        div.innerHTML = `
-        <a onclick="projeto()">
-            <div class="d-flex flex-column col-md-3 configBox m-5">
-                <img src="${projeto.imagem}" alt="memoro">
-                <p class="text-light">${projeto.texto}</p>
-            </div>
-        </a>`;
-
+    // Adiciona classes à div
+    div.classList.add('d-flex', 'flex-column','btn', 'col-md-3', 'configBox', 'm-5','container','w-40');
+    
+    // Define o conteúdo da div
+    div.innerHTML = `
+      <img src="${exemplo.imagem}" alt="${exemplo.nome}" class="container">
+      <p class="text-light">${exemplo.texto}</p>
+      
+    `;
+    console.log(exemplo);
+    div.addEventListener('click', () => projeto(exemplo));
+    
     // Adiciona a div ao container
     container.appendChild(div);
-});
+  });
+}
--- rotas/web.js
+++
@@ -1,4 +0,0 @@
-
-function home(){
-    window.location.href = "index.html"; 
-}
